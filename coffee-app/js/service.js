cf7ff4896a72070b167c07be46a497c0f266b7a6
fazi1live <faziislive@gmail.com>
2022-04-21 11:39:33 +0500

    add

--- coffee-app/js/service.js
+++ coffee-app/js/service.js
@@ -2,7 +2,14 @@ let UserData = JSON.parse(localStorage.getItem('user-object'));
 
 UserData.forEach((element, i) => {
     let DivToShow = document.querySelector('.table-data');
-    let TableData = document.createElement('td');
-    TableData.appendChild(document.createTextNode(`${element.Name}`));
-    DivToShow.appendChild(TableData);
+    DivToShow.innerHTML += `
+    <tr>
+    <td>${i+1}</td>
+    <td>${element.Name}</td>
+    <td>${element.Email}</td>
+    <td>${element.Coffee}</td>
+    <td>${element.House}</td>
+    <td>${element.Phone}</td>
+    </tr>
+    `
 });
