0131dc3d5b62bbb3f90fc88c29d85450a68986a4
fazi1live <faziislive@gmail.com>
2022-09-09 17:54:51 +0500

    updated code

--- e-commerce-application/front-end/src/app/admin-module/admin-module.module.ts
+++ e-commerce-application/front-end/src/app/admin-module/admin-module.module.ts
@@ -3,15 +3,22 @@ import { CommonModule } from '@angular/common';
 
 import { AdminModuleRoutingModule } from './admin-module-routing.module';
 import { AdminModuleComponent } from './admin-module.component';
+import { DashboardComponent } from './admin-components/dashboard/dashboard.component';
+import { ProductsComponent } from './admin-components/products/products.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 
 @NgModule({
   declarations: [
     AdminModuleComponent,
+    DashboardComponent,
+    ProductsComponent
   ],
   imports: [
     CommonModule,
-    AdminModuleRoutingModule
+    AdminModuleRoutingModule,
+    FormsModule,
+    ReactiveFormsModule
   ]
 })
 export class AdminModuleModule { }
