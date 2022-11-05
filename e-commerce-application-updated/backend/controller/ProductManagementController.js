9ca64474324473b626aab6f85a74bd8b3c99f440
fazi1live <faziislive@gmail.com>
2022-11-05 19:09:17 +0500

    add

--- e-commerce-application-updated/backend/controller/ProductManagementController.js
+++ e-commerce-application-updated/backend/controller/ProductManagementController.js
@@ -5,8 +5,6 @@ const ProductData = async (req, res) => {
         const { productName, quantity, price, description, color, companyName, category, size, productMaterial } =req.body;
         let ImageDetaisl = [];
         let Size = size.split(',');
-        size;
-        return
         req.files.forEach(arrayObject => {
             const { filename, originalname, mimetype } = arrayObject;
             ImageDetaisl.push({
@@ -27,7 +25,6 @@ const ProductData = async (req, res) => {
             Data:true
         })
     } catch (error) {
-        console.log(error)
         res.json({
             Message: error.mesage,
             Result: null,
